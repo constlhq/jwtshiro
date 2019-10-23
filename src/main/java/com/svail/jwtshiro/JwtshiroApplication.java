a1cf2c2b5862fcac359ed64b4adb981b3e02f042
lhq <const.lhq@gmail.com>
2019-10-23 17:00:01 +0800

    '创建项目'

---
+++ src/main/java/com/svail/jwtshiro/JwtshiroApplication.java
@@ -0,0 +1,13 @@
+package com.svail.jwtshiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtshiroApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JwtshiroApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/svail/jwtshiro/JwtshiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svail.jwtshiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtshiroApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
