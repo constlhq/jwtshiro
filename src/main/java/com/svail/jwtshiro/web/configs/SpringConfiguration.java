5915e0f796556b0ac620b776db77a0dbb808b56a
han qing <const.lhq@gmail.com>
2019-10-30 07:23:57 +0800

    一点点

--- src/main/java/com/svail/jwtshiro/web/configs/SpringConfiguration.java
+++ src/main/java/com/svail/jwtshiro/web/configs/SpringConfiguration.java
@@ -2,6 +2,8 @@ package com.svail.jwtshiro.web.configs;
 
 import com.svail.jwtshiro.shiro.realms.UsernameRealm;
 import com.svail.jwtshiro.shiro.services.IAccountProvider;
+import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
+import org.apache.shiro.crypto.hash.Hash;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,7 +14,20 @@ public class SpringConfiguration {
   @Bean
   public AuthorizingRealm userNameRealm(IAccountProvider accountProvider){
     UsernameRealm usernameRealm = new UsernameRealm();
-    usernameRealm.setAccountProvider();
+    usernameRealm.setAccountProvider(accountProvider);
+
+    return usernameRealm;
+
   }
+
+  @Bean
+  public HashedCredentialsMatcher hashedCredentialsMatcher(){
+    HashedCredentialsMatcher hashedCredentialsMatcher = new HashedCredentialsMatcher();
+    hashedCredentialsMatcher.setHashAlgorithmName("SHA256");
+    hashedCredentialsMatcher.setHashIterations(2);
+    hashedCredentialsMatcher.setStoredCredentialsHexEncoded(true);
+    return hashedCredentialsMatcher;
+  }
+
 
 }
